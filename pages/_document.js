97d9675045a6d9577418bcd3ae29cc77e8e712d5
srcnzn <sercanozen1@gmail.com>
2025-05-27 01:15:29 +0200

    Fix viewport and scaling issues

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document"
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta charSet="utf-8" />
+      </Head>
       <body>
         <Main />
         <NextScript />

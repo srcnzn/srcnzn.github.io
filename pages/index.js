425bfbaf1206a3a064d8fc9d85e193c2f523ba83
srcnzn <sercanozen1@gmail.com>
2025-05-27 00:49:33 +0200

    Move all pages to root level

--- pages/index.js
+++ pages/index.js
@@ -11,37 +11,30 @@ export default function Page() {
     <main className="unified-layout">
       <Navbar />
 
-      {/* Home Section */}
       <section id="home" className="section-container bg-gradient-to-br from-gray-900 via-blue-900 to-gray-900">
         <Home />
       </section>
 
-      {/* About Section */}
       <section id="about" className="section-container">
         <About />
       </section>
 
-      {/* Studies Section */}
       <section id="studies" className="section-container bg-gradient-to-br from-purple-900 to-gray-900">
         <Studies />
       </section>
 
-      {/* Analysis Tools Section (Data) */}
       <section id="data" className="section-container">
         <Data />
       </section>
 
-      {/* Characterization Section */}
       <section id="characterization" className="section-container">
         <Experimental />
       </section>
 
-      {/* Contact Section */}
       <section id="contact">
         <Contact />
       </section>
 
-      {/* Footer */}
       <footer className="bg-gray-900 text-white py-8">
         <div className="max-w-7xl mx-auto px-6 text-center">
           <p>&copy; 2024 Sercan Ozen. All rights reserved.</p>
